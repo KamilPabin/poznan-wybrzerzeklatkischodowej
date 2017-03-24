24f833353a0203ee75e4ea56f08af24735bf6045
Kacper Koza <kkoza11@gmail.com>
2017-03-24 21:28:22 +0100

    Methods with speech recognizer. Returns List of matches

--- app/src/main/java/braincode/com/smartsearch/SearchFragment.java
+++ app/src/main/java/braincode/com/smartsearch/SearchFragment.java
@@ -2,19 +2,28 @@ package braincode.com.smartsearch;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+import static android.app.Activity.RESULT_OK;
+
 public class SearchFragment extends Fragment {
+
+    private static final int SPEECH_REQUEST_CODE = 0;
 
     @BindView(R.id.fab)
     FloatingActionButton fab;
@@ -28,8 +37,32 @@ public class SearchFragment extends Fragment {
         return view;
     }
 
+
+    // Create an intent that can start the Speech Recognizer activity
     @OnClick(R.id.fab)
-    public void startUsingSpeechRecognizer() {
+    public void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        startActivityForResult(intent, SPEECH_REQUEST_CODE);
+    }
+
+    //Callback from intent. Get text from intent content
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+
+            for (String s : results) {
+                Log.d("spokenText", s);
+            }
+            // Do something with spokenText
+        }
 
     }
 
