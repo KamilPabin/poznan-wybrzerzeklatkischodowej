c3f78c816127b23632640c1267630ea010658879
Kacper Koza <kkoza11@gmail.com>
2017-03-24 20:39:46 +0100

    fab + searchFragment

--- app/src/main/java/braincode/com/smartsearch/MainActivity.java
+++ app/src/main/java/braincode/com/smartsearch/MainActivity.java
@@ -1,13 +1,22 @@
 package braincode.com.smartsearch;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+
+    private android.app.FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction
+                .add(R.id.frame_layout, new SearchFragment())
+                .commit();
     }
 }
