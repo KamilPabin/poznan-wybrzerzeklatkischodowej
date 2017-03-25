9b156b2982071b55c3db3fe8e56f30537df3b06a
Kamil Pabin <kamil.pabin@gmail.com>
2017-03-25 07:41:40 +0100

    Data from API binded with UI 2

--- app/src/main/java/braincode/com/smartsearch/ShowResultsFragment.java
+++ app/src/main/java/braincode/com/smartsearch/ShowResultsFragment.java
@@ -1,5 +1,6 @@
 package braincode.com.smartsearch;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,18 +9,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
+import braincode.com.smartsearch.Model.Item;
+
 /**
  * Created by Little on 2017-03-24.
  */
 
 public class ShowResultsFragment extends Fragment {
 
+    private Context context;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View v = super.onCreateView(inflater, container, savedInstanceState);
         RecyclerView recyclerView = (RecyclerView) v.findViewById(R.id.SearchResultList);
-        recyclerView.setAdapter(new ResultListAdapter(null, getContext()));
+
+        List<Item> Data = (List<Item>)this.getArguments().get("Items");
+        recyclerView.setAdapter(new ResultListAdapter(Data, context));
         return v;
     }
 }
