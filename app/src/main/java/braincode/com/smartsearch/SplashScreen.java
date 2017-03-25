89ad40ca3018a92de6c7b3354c3b67eb4d3ea8ca
Kamil Pabin <kamil.pabin@gmail.com>
2017-03-25 14:47:34 +0100

    Added animations

--- app/src/main/java/braincode/com/smartsearch/SplashScreen.java
+++ app/src/main/java/braincode/com/smartsearch/SplashScreen.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 /**
  * Created by Little on 2017-03-25.
@@ -16,6 +19,16 @@ public class SplashScreen extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen_layout);
+
+        TextView T1 = (TextView) findViewById(R.id.T1);
+        Animation anim1 = AnimationUtils.loadAnimation(this,R.anim.text1_move);
+        T1.startAnimation(anim1);
+
+        TextView T2 = (TextView) findViewById(R.id.T2);
+        Animation anim2 = AnimationUtils.loadAnimation(this,R.anim.text2_move);
+        T2.startAnimation(anim2);
+
+
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
